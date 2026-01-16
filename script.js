e540230614014a31236429fa3b23ccb3a44dfa5e
Rohit Prasad <pp0549353@gmail.com>
2026-01-16 10:24:48 +0530

    Fifth commit

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-let allElems = document.querySelectorAll('.elem');
+function openFeatures() {
+  let allElems = document.querySelectorAll(".elem");
 
-let fullElemPage = document.querySelectorAll('.fullElem');
+  let fullElemPage = document.querySelectorAll(".fullElem");
 
-// backbutton code 
-let allFullElemBackBtn =  document.querySelectorAll('.fullElem .back');
+  // backbutton code
+  let allFullElemBackBtn = document.querySelectorAll(".fullElem .back");
 
-allElems.forEach(function(elem){
-    elem.addEventListener('click', function(){
-        fullElemPage[elem.id].style.display = 'block'
+  allElems.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      fullElemPage[elem.id].style.display = "block";
     });
-})
+  });
 
-// now writing the logic of back button
-allFullElemBackBtn.forEach(function(back){
-    back.addEventListener('click', function(){
-        fullElemPage[back.id].style.display = 'none'
-    })
-});
+  // now writing the logic of back button
+  allFullElemBackBtn.forEach(function (back) {
+    back.addEventListener("click", function () {
+      fullElemPage[back.id].style.display = "none";
+    });
+  });
+}
+
+openFeatures();
+
+
+//part 02 Todo Page
